214d622ed4cabd027aee3108a8d59bb183768584
Harrison Crisapulli <harrisonc2105@gmail.com>
2026-07-20 21:32:45 +1000

    chore: swap Cormorant Garamond for Lora

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import '@fontsource/cormorant-garamond/500.css'
-import '@fontsource/cormorant-garamond/600.css'
-import '@fontsource/cormorant-garamond/500-italic.css'
+import '@fontsource/lora/500.css'
+import '@fontsource/lora/600.css'
+import '@fontsource/lora/500-italic.css'
 import './styles/theme.css'
 
 createRoot(document.getElementById('root')!).render(
